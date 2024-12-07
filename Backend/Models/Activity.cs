6559b15f205814847f7e8fd0dac3c8c312c1be36
SilasPeters <69711739+SilasPeters@users.noreply.github.com>
2024-12-07 23:33:05 +0100

    feat: add first models

---
+++ Backend/Models/Activity.cs
@@ -0,0 +1,23 @@
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+namespace Backend.Models;
+
+public class Activity
+{
+    /// <summary>
+    /// The unique identifier of an activity, assigned incrementally.
+    /// </summary>
+    public uint   Id          { get; set; }
+    /// <summary>
+    /// The name of the activity.
+    /// </summary>
+    public string Name        { get; set; }
+    /// <summary>
+    /// A description or arbitrary length, explaining everything there is to know about the activity.
+    /// </summary>
+    public string Description { get; set; } // TODO: this is of course not good for localisation
+
+    /// <summary>
+    /// The date and time at which the activity will start.
+    /// </summary>
+    public DateTimeOffset DateTimeStart { get; set; }
+}
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,3 +1,5 @@
+using Backend.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,29 +18,17 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-	"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-	{
-		var forecast = Enumerable.Range(1, 5).Select(index =>
-				new WeatherForecast
-				(
-					DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-					Random.Shared.Next(-20, 55),
-					summaries[Random.Shared.Next(summaries.Length)]
-				))
-			.ToArray();
-		return forecast;
-	})
+app.MapGet("/activity", (uint activityId) =>
+    {
+        return new Activity()
+        {
+            Id = activityId,
+            Name = "Some activity",
+            Description = "Some activity description",
+            DateTimeStart = DateTimeOffset.Now,
+        };
+    })
 	.WithName("GetWeatherForecast")
 	.WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
