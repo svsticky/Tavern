72341d8875492f99b62423e140f59f260e7f7577
Rens van Moorsel <rens.van.moorsel@gmail.com>
2025-11-26 15:46:19 +0000

    Setup for frontend

---
+++ Frontend/src/router/index.tsx
@@ -0,0 +1,14 @@
+import HomePage from '@/Views/HomePage';
+import MainLayout from '@/Layouts/MainLayout.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+
+    },
+  ],
+});
+
+export default router;
