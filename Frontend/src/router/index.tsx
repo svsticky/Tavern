fd064f4cafaef4632b9f97a070679c636a728e7f
Rens van Moorsel <rens.van.moorsel@gmail.com>
2025-11-26 15:47:05 +0000

    Remove code for another pr

--- Frontend/src/router/index.tsx
+++ Frontend/src/router/index.tsx
@@ -1,13 +1,8 @@
-import HomePage from '@/Views/HomePage';
-import MainLayout from '@/Layouts/MainLayout.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {
-
-    },
   ],
 });
 
