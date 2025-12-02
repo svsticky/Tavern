ef72699eebb3f7d49956f2dfe457e3da1013c0f6
Mees Maas <meesmaas999@gmail.com>
2025-12-02 15:16:29 +0100

    ci: adjust biome

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
