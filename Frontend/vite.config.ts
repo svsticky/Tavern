05c5ac9c08e943457140ec8180e169322d981fe6
Mees Maas <meesmaas999@gmail.com>
2025-12-09 18:36:24 +0100

    feat: boilerplate

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,24 +1,8 @@
-import path from "node:path";
+import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-    strictPort: true,
-    watch: {
-      usePolling: true,
-      interval: 100,
-    },
-  },
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
