3344415a7a5334ab4175410db958db6781622588
Rens van Moorsel <r.v.moorsel@primevision.com>
2026-02-10 11:35:00 +0000

    Migration

---
+++ Backend/Migrations/20260210113403_ActivitiesWithNullableValuesAndAllowedAudience.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActivitiesWithNullableValuesAndAllowedAudience : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_Groups_OrganizerId",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "IsOpenToFirstYears",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "IsOpenToMasters",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "IsOpenToSecondYears",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "IsOpenToThirdYearsAndAbove",
+                table: "Activities");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "VatRate",
+                table: "Activities",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UnenrollmentDeadline",
+                table: "Activities",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "OrganizerId",
+                table: "Activities",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EnglishDescription",
+                table: "Activities",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(240)",
+                oldMaxLength: 240);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DutchDescription",
+                table: "Activities",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(240)",
+                oldMaxLength: 240);
+
+            migrationBuilder.AddColumn<long>(
+                name: "AllowedAudience",
+                table: "Activities",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "EnrollmentDeadline",
+                table: "Activities",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_Groups_OrganizerId",
+                table: "Activities",
+                column: "OrganizerId",
+                principalTable: "Groups",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_Groups_OrganizerId",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "AllowedAudience",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "EnrollmentDeadline",
+                table: "Activities");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "VatRate",
+                table: "Activities",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UnenrollmentDeadline",
+                table: "Activities",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "OrganizerId",
+                table: "Activities",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EnglishDescription",
+                table: "Activities",
+                type: "character varying(240)",
+                maxLength: 240,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DutchDescription",
+                table: "Activities",
+                type: "character varying(240)",
+                maxLength: 240,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOpenToFirstYears",
+                table: "Activities",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOpenToMasters",
+                table: "Activities",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOpenToSecondYears",
+                table: "Activities",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOpenToThirdYearsAndAbove",
+                table: "Activities",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_Groups_OrganizerId",
+                table: "Activities",
+                column: "OrganizerId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
