3629f051606a9597773e222475b38f087a9e6606
Rens van Moorsel <rens.van.moorsel@gmail.com>
2026-05-13 10:04:12 +0000

    fix: load correct languaage on register page

--- Frontend/app/i18n/index.ts
+++ Frontend/app/i18n/index.ts
@@ -1,15 +1,20 @@
 import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import HttpBackend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
 i18n
+  .use(LanguageDetector)
   .use(HttpBackend)
   .use(initReactI18next)
   .init({
+    supportedLngs: ["en", "nl"],
+    nonExplicitSupportedLngs: true,
+    load: "languageOnly",
     fallbackLng: "en",
     debug: true,
     detection: {
-      order: ["querystring", "cookie", "localStorage", "navigator"],
+      order: ["querystring", "navigator", "cookie", "localStorage"],
       caches: ["localStorage", "cookie"],
     },
     backend: {
