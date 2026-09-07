ffa8f2423cbd260434b0148760a8275a9ca39dc4
Rensvm <github@rensvanmoorsel.nl>
2026-09-07 12:11:52 +0200

    Fix/dont sent mails via hangfire that are already sent (#96)

--- Backend/Migrations/20260907075619_AddMemberStudyStatusMailSentAt.Designer.cs
+++ Backend/Migrations/20260907075619_AddMemberStudyStatusMailSentAt.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Backend.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Backend.Migrations
 {
     [DbContext(typeof(PostgresDbContext))]
-    partial class PostgresDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260907075619_AddMemberStudyStatusMailSentAt")]
+    partial class AddMemberStudyStatusMailSentAt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -485,9 +488,6 @@ namespace Backend.Migrations
 
                     b.Property<string>("Notes")
                         .HasColumnType("text");
-
-                    b.Property<DateTimeOffset?>("OutstandingPaymentMailSentAt")
-                        .HasColumnType("timestamp with time zone");
 
                     b.Property<string>("ParentPhoneNumber")
                         .HasColumnType("text");
